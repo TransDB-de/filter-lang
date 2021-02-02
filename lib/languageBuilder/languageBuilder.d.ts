97ac19fc710cecb186c4228da2d30bc8736ac176
ElectronicBlueberry <44241786+ElectronicBlueberry@users.noreply.github.com>
2021-02-02 23:00:08 +0100

    removed compiled from repository

--- lib/languageBuilder/languageBuilder.d.ts
+++
@@ -1,8 +0,0 @@
-import type { LanguageFilterDictionary } from "../types/internal";
-import type { FilterDefinition, LanguageDefinition, StringDictionary } from "../types/languageDefinition";
-export type { FilterDefinition, LanguageDefinition, StringDictionary };
-/**
- * Scans the provided language definition, and changes it into a format more suitable for parsing
- * @param definition
- */
-export declare function makeLangFilterDictionary(definition: LanguageDefinition): LanguageFilterDictionary;
--- lib/util.d.ts
+++
@@ -1,8 +0,0 @@
-import type { StringDictionary } from "./types/languageDefinition";
-export declare function lowerCaseKeys(dict: StringDictionary): StringDictionary;
-/**
- * Deep copies are made to avoid mutating the user input,
- * and to decouple the compiled / parsed data from any potentially mutable data
- * @param obj obj to deep copy
- */
-export declare function deepCopy<T>(obj: T): T;
